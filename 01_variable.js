5940c1bfcaa39a575050853f36553b6ed36ebe5a
VickyWadwane <vikrantwadwane12@gmail.com>
2023-07-24 18:53:50 +0530

    Basic Variable programs
    Basic programs of variables

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+
+
+var myName; //Variable Declartation
+myName="Vikrant Bholanath Wadwane"; //Variable Assignment or Initialization
+
+console.log(myName);
+
+var city = "Pune";//Variable Decleration & Initialization
+console.log(city);
+
+var pin = "415539;"
+console.log(pin);
