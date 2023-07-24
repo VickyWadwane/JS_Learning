80a5b149a914f8bc2e2cd7b1c51f16c4e6136aaf
VickyWadwane <vikrantwadwane12@gmail.com>
2023-07-24 20:39:35 +0530

    Variable & Type of Operator programs
    Variable & Type of Operator programs

---
+++ 02_variableBasics_assign.js
@@ -0,0 +1,16 @@
+console.log("---------------Statement 1-------------");
+
+var companyName = "CodeSeed Solution";
+console.log(companyName);
+console.log(typeof companyName);
+
+var isWFH = true;
+console.log(isWFH);
+console.log(typeof isWFH);
+
+var phoneNumber = 7248996044;
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+
+console.log("---------------Statement 2-------------");
+
