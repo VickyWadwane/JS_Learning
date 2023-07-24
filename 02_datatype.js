1318b20c7b3e6c342dfdfd7f6be2e9552b8d76c8
VickyWadwane <vikrantwadwane12@gmail.com>
2023-07-24 19:20:40 +0530

    datatype basic programs
    datatype basic programs

---
+++ 02_datatype.js
@@ -0,0 +1,16 @@
+
+var firstName = "Vikrant";
+console.log(firstName);
+console.log(typeof(firstName));
+
+var age = 25;
+console.log(age);
+console.log(typeof age);
+
+var isMarried = false;
+console.log(isMarried);
+console.log(typeof isMarried);
+
+var city = "Pune";
+console.log(city);
+console.log(typeof city);
