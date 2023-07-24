030d94751b2687eb0f3f722f7fcfd92be23374de
VickyWadwane <vikrantwadwane12@gmail.com>
2023-07-24 20:52:35 +0530

    Type of operator, variable, datatype
    Type of operator, variable, datatype

---
+++ 03_typeOf.js, Variable, data type and typeof operator_assign.js
@@ -0,0 +1,37 @@
+
+console.log("-----------------------Statement 1----------------------");
+
+console.log("---------------Before Initialization-------------");
+
+var bankName;
+console.log(bankName);
+console.log(typeof bankName);
+
+console.log("---------------After Initialization-------------");
+
+bankName = "SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+
+console.log("-----------------------Statement 2----------------------");
+var marks = '90%';
+console.log(marks);
+console.log(typeof marks);
+
+console.log("-----------------------Statement 3----------------------");
+var isWorking;
+console.log(isWorking);
+isWorking = true;
+console.log(isWorking);
+console.log(typeof isWorking);
+
+console.log("-----------------------Statement 4----------------------");
+
+var totalCount = "Hundred And Seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+
+
+totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
