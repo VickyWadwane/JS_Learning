ad851e2d51013ee2c14ed557ee0ed155d2733c85
VickyWadwane <vikrantwadwane12@gmail.com>
2023-07-24 20:29:49 +0530

    Assignment 1
    Variable Assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,40 @@
+console.log("------------------Statement 1----------------");
+
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Vikrant Wadwane";
+console.log(myFullName);
+
+myFullName = "Vikrant Bholanath Wadwane";
+console.log(myFullName);
+
+console.log("------------------Statement 2----------------");
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+
+console.log("------------------Statement 3----------------");
+
+var firstName = "Vikrant";
+console.log(firstName);
+
+var lastName = "Wadwane";
+console.log(lastName);
+
+var age = 25;
+console.log(age);
+
+var address = "Plot no 2, Ayodhyanagari Agashivnagar Karad";
+console.log(address);
+
+var pincode = 415539;
+console.log(pincode);
+
+var gender = "Male";
+console.log(gender);
+
+var isMarried = true;
+console.log(isMarried);
+
