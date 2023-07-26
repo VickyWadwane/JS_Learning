666eb20c7a665db35142a585219f629da9172481
VickyWadwane <vikrantwadwane12@gmail.com>
2023-07-26 12:56:30 +0530

    Swapping Variable Assignment
    assignment of Swapping Variables

---
+++ 01_swapVariableAssign.js
@@ -0,0 +1,29 @@
+
+console.log("--------------------------Step 1------------------------");
+console.log("Program Starts Here");
+
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Values Before Swap->","sweety =",sweety,"&","cutie =",cutie);
+
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("Values After Swap->","sweety =",sweety,"&","cutie =",cutie);
+console.log("Program Ends Here");
+
+console.log("--------------------------Step 2------------------------");
+console.log("Program Starts Here");
+
+var num1 =100;
+var num2 =200;
+var num3 =300;
+console.log("Values Before Swap->","num1 =",num1,"&","num2 =",num2,"&","num3 =",num3);
+
+var temp = num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("Values After Swap->","num1 =",num1,"&","num2 =",num2,"&","num3 =",num3);
+
+console.log("Program Ends Here");
