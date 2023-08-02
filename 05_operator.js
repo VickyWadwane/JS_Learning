7cde7335e011b4bfa585ed3eff7561d7fad36334
VickyWadwane <vikrantwadwane12@gmail.com>
2023-08-02 16:25:51 +0530

    Function Expression and Basic of Operators in Javascript
    Function Expression and Basic of Operators in Javascript

---
+++ 05_operator.js
@@ -0,0 +1,34 @@
+
+
+
+
+// + Addition
+// - Substraction
+// * Multiplication
+console.log(`----------------Exponential Operator----------------`);
+var num1 =10;
+var num2 = 3;
+var result = num1 ** num2; //Exponential Operator
+console.log(`Result is ${result}`);
+
+console.log(`----------------Division Operator----------------`);
+var result = num1/num2;
+console.log(`Result is ${result}`);
+
+console.log(`----------------Modulus Operator----------------`);
+var result = num1%num2;
+console.log(`Result is ${result}`);
+console.log(`${15%3}`);
+console.log(`${12%2}`);
+
+console.log(`-----------------------Assignment Operators------------------`);
+var num1 = 10;
+num1+=5;
+console.log(`Result is : ${num1}`);
+num1-=5;
+console.log(`Result is : ${num1}`);
+num1*=5;
+console.log(`Result is : ${num1}`);
+
+
+
