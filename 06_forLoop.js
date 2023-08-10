a93c54c50d67a4af597e3aca1b40de942931be67
VickyWadwane <vikrantwadwane12@gmail.com>
2023-08-10 10:22:53 +0530

    for loop with for assignment
    for loop with for assignment

--- 06_forLoop.js
+++ 06_forLoop.js
@@ -1,14 +1,51 @@
+var num = 10;
+var num2 = num++; // num2 = num + 1
+console.log(`Value of num : ${num}`);
+console.log(`Value of num2 : ${num2}`);
+
+var num4 = 10;
+var num5 = ++num4; // num4 = num4 + 1
+console.log(`Value of num : ${num4}`);
+console.log(`Value of num2 : ${num5}`);
+
+ 
+
+var num6 = 5;
+var num7 = --num6; // num7 = num6 - 1
+console.log(`Value of num6 : ${num6}`);
+console.log(`Value of num7 : ${num7}`);
 
 
+console.log(`================================`);
 
-//WAP to print numbers from 0 to 10
-//start =0
-//condition <=0
-//increment = 1
+// WAP to print numbers from 0 to 10
+// start index = 2
+// condition: <= 1
+// increment = 1
+
+ 
 
 for (let index = 0; index <= 10; index++) {
-    console.log(index);
-    
+    console.log(index); 
 }
+console.log(`---------------------------`);
+// Log numbers from 5 to 20
+for (let index = 5; index <=20; index++) {
+    console.log(index);
+}
+
+
+
+console.log(`---------------------While loop-------------------`);
+//Numbers from 1 to 10;
+var i =1 ;
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+
+
+
+
 
 
---
+++ 06_for_Assignment1.js
@@ -0,0 +1,53 @@
+
+console.log(`---------------------------------Step 1-------------------------`);
+
+for (let index = 5; index <=15; index++){
+    console.log( index);
+   
+}
+
+console.log(`---------------------------------Step 2-------------------------`);
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);    
+}
+
+console.log(`---------------------------------Step 3-------------------------`);
+//first 15 odd numbers ;
+console.log(`First 15 Odd Numbers are:`);
+for (let index = 1; index<30; index=index+2) {
+    console.log(index);    
+}
+
+console.log(`---------------------------------Step 4-------------------------`);
+// first 15 even numbers;
+console.log(`First 15 Even Numbers are:`);
+for (let index = 2; index <=30; index = index+2) {
+    console.log(index);
+}
+
+console.log(`---------------------------------Step 5-------------------------`);
+//write the table of 5
+console.log(`The table of 5 is as below:`);
+for (let index = 5; index <=50; index = index+5) {
+    console.log(index);
+    
+}
+
+console.log(`---------------------------------Step 6--------------------------`);
+//write the table of 10
+console.log(`The table of 10 is as below:`);
+for (let index = 10; index <=100; index = index+10) {
+    console.log(index);
+}
+
+console.log(`---------------------------------Step 7-------------------------`);
+//write the table of 10 in reverse order
+console.log(`The table of 10 in reverse order:`);
+for (let index = 100; index >=10; index = index-10) {
+    console.log(index);
+    
+}
+
+
+
