c04b05bdc8bfce3f9e180df747ad260453878d90
VickyWadwane <vikrantwadwane12@gmail.com>
2023-08-16 09:42:38 +0530

    vowels count & nodejs

---
+++ 06_vowelsCount.js
@@ -0,0 +1,12 @@
+
+
+
+
+function vowelCount(str){
+   let char = str.charAt(0);
+   console.log(char);
+   if(char==(a,e,i,o,u)|| char == (A,E,I,O,U)){
+        console.log(`It is a vowel`);
+   }
+}
+vowelCount("I am very good UI Developer");
