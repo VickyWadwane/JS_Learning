e77d513e5489f06705bcd6bd6a4510a2d1f62e38
VickyWadwane <vikrantwadwane12@gmail.com>
2023-08-22 17:55:44 +0530

    Rest Parameter, Higher order function, arrow function, for each loop
    Rest Parameter, Higher order function, arrow function, for each loop

--- 16_restParameter.js
+++ 16_restParameter.js
@@ -1,6 +1,6 @@
 function add(num1, num2, ...num ){
-    // console.log(`num1 ${num1},  num2 ${num2}`);
-    // console.log(num);
+    console.log(`num1 ${num1},  num2 ${num2}`);
+    console.log(num);
 }
 add(5, 6);
 add(9, 5, 6);
